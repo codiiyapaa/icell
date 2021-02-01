5db4f55850f14be0ea32095124117059de7c9e4a
Paras Bansal <parasbansal685@gmail.com>
2021-02-01 14:19:16 +0530

    Login Page Done

--- icell/src/components/Loginpage.jsx
+++ icell/src/components/Loginpage.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
- 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+
 const Loginpage = () => {
     return (
-       <div>
-          <h1>Loginpage</h1>
-           <p>Loginpage body content</p>
-       </div>
+
+    <div style={{ display:'flex', justifyContent:'center' }}>
+        <Card style={{ width: '28rem', height: '20rem', borderColor: 'black', backgroundColor: '#eceeee'}} >
+        <Card.Header style={{textAlign: 'center', borderColor: 'black', fontWeight: 'bold', fontSize: '17pt'}}>Login</Card.Header>
+        <Card.Body>
+            <Card.Text>
+            <Form>
+                <Form.Group controlId="formBasicUsername">
+                <Form.Row>
+                    <Col xs={10}>
+                        <Form.Control style={{borderColor: 'black'}} type="Username" placeholder="Username" />
+                    </Col>
+                </Form.Row>
+                </Form.Group>
+                <br></br>
+                <Form.Group controlId="formBasicPassword">
+                <Form.Row>
+                    <Col xs={10}>
+                        <Form.Control style={{borderColor: 'black'}} type="password" placeholder="Password" />
+                    </Col>
+                </Form.Row>
+                </Form.Group>
+                <br></br>
+                <Button style={{backgroundColor: '#37a69b', fontWeight: 'bold'}} type="submit" size="lg">
+                    SIGN IN
+                </Button>
+            </Form>
+            </Card.Text>
+        </Card.Body>
+        </Card>
+    </div>
     );
 }
  
--- icell/src/components/Navigation.jsx
+++ icell/src/components/Navigation.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Navigation.css';
-
+import Navbar from 'react-bootstrap/Navbar'
 
 function Navigation() {
     var x=-1;
     console.log(x);
     return(
         <>
-            <div className="navbar">
-                <nav>
-                    Admin Panel
-                </nav>
-            </div>
+        <Navbar>
+            <Navbar.Toggle />
+            <Navbar.Collapse className="justify-content-center">
+            <Navbar.Text style={{color: '#FFFFFF', fontSize: '40pt', fontWeight: 'bold'}}>
+                Admin Panel- Industry Cell Website
+            </Navbar.Text>
+            </Navbar.Collapse>
+        </Navbar>
+        <br></br>
         </>
     );
 }
